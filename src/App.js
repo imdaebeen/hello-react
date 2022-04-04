d588505727cf863422d3c9c7cad50850bd7adccc
imdaebeen <imdaebeen_09@naver.com>
2022-04-04 23:52:46 +0900

    JSX

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
+ //번들러 사용 웹팩,Parcel,browserify 불러온 모듈을 모두 합쳐서 하나의 파일을 생성 
 import './App.css';
+//import { Fragment } from 'react';
+
+// function App() {
+//   const name = '리웩트';
+//   return <div>{name === '리액트' && <h1>리액트 입니다.!</h1> }</div>; //조건부
+//     /*
+//     함수 컴포넌트
+//     JSX코드 HTML태그 쓰듯이 작성
+//     */
+// }
+// function App() {
+//   const name = '리액트';
+//   const style = {
+//     // background-color는 backgroundColor와 같이 -가 사라지고 카메 표기법으로 작성된다.
+//     backgroundColor : 'black',
+//     color: 'aqua',
+//     fontSize:'48px',//font-size-> fontSize
+//     fontWeight:'bold',//font-weight-> fontWeight
+//     padding:16 // 단위를 생략하면 px로 지정됨.
+//   };
+
+//   return <div style={style}>{name}</div>
+// }
+
 
 function App() {
+  const name = '리액트';
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+  <div className='react'>{name}</div>
+  {/*주석은 이렇게 작성한다. */}
+  //이런주석
+  /* 요런주석*/
+  <input />
+  </>
   );
 }
 
